0bf6ac45f93cd9463eb393955b77919b5263ffef
JackDunne <8jdunne@stpatrick.org>
2017-09-11 14:41:51 -0500

    worked

--- wave.js
+++ wave.js
@@ -1,22 +1,52 @@
 const Point = require('./point.js');
 const Wave = function(array){
-      let numberOfPoints = array.length;
-      let points = array;
-      let transformedPoints = array;
-      let range =
-      let period =
+    let numberOfPoints = array.length;
+    let points = array;
+    let transformedPoints = array;
+    let range =
+    let period =
 
-      function getRange(){return range;}
-      function getPeriod(){return period;}
-      function getPointArray(){return points;}
+    function getRange(){return range;}
+    function getPeriod(){return period;}
+    function getPointArray(){return points;}
 
-      function shiftX(value){
-      //take the x points and add or subtract numbers(value) to shift it
-      //loop through list of points, get the x, ask first point who it is, save x and save y, then update the x, this is all inside the for loop
-      for(a=0;a<numberOfPoints;a++){
-          
-      }
-      }
+    function shiftX(value){
+        for(a=0;a<numberOfPoints;a++){
+            let tempX = p1.getX();
+            let tempY = p1.getY();
+            tempX += value;
+            let tempPoint = new Point(tempX,tempY);//this stuff probably isnt correct
+            for(let x = 0;x<transformedPoints.length;x++){
+                transformedPoints[x] =
+            }
+        }
+    }
+
+    function shiftY(value){
+        for(a=0;a<numberOfPoints;a++){
+            let tempX = p1.getX();
+            let tempY = p1.getY();
+            tempY += value;
+            let tempPoint = new Point(tempX,tempY);
+        }
+    }
+
+    function StretchX(value){
+        for(a=0;a<numberOfPoints;a++){
+            let tempX = p1.getX();
+            let tempY = p1.getY();
+            tempX = tempX*value;
+            let tempPoint = new Point(tempX,tempY);
+        }
+    }
+
+    function StretchY(value){
+        for(a=0;a<numberOfPoints;a++){
+            let tempX = p1.getX();
+            let tempY = p1.getY();
+            tempY = tempY*value;
+            let tempPoint = new Point(tempX,tempY);
+        }
+    }
 }
-
 module.exports = Wave;
